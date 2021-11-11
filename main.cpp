acb060a55f4f6a027317f664e363489eafaf8564
FedericaBisi <91806456+FedericaBisi@users.noreply.github.com>
2021-11-11 19:57:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+   int n=0; int primo=1; int secondo=1; int terzo=0;
+   cin>>n;
+   if(n>=2){
+      cout<<primo<<endl;
+      cout<<secondo<<endl;
+      for(int i=2; i<n; i++){
+         terzo=primo+secondo;
+         primo=secondo;
+         secondo=terzo;
+         cout<<terzo<<endl;
+      }
+   }else{
+      cout<<"errore"<<endl;
+   }
    return 0;
 }
